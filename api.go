779cd79e69ab9de9cc93eb4c9564c4d84cbb89ee
raphael <dr.marduk@gmail.com>
2017-03-06 19:09:22 +0100

    refactor to a new file

--- api.go
+++ api.go
@@ -2,39 +2,8 @@ package main
 
 import (
 	"database/sql"
-	"errors"
-	"fmt"
 	"log"
-	"strconv"
-	"time"
 )
-
-// Record holds a current weight
-type Record struct {
-	ID      int
-	Created time.Time
-	Value   float32
-}
-
-func (r *Record) String() string {
-	return fmt.Sprintf("%d - %.2f (%s)", r.ID, r.Value, r.Created.Format(time.UnixDate))
-}
-
-// ParseRecord returns a fully parsed record based on the input string
-// and adds the created member
-func ParseRecord(value string) (r Record, err error) {
-	if value == "" {
-		return r, errors.New("empty value")
-	}
-	x, err := strconv.ParseFloat(value, 32)
-	if err != nil {
-		return r, err
-	}
-
-	r.Created = time.Now()
-	r.Value = float32(x)
-	return
-}
 
 // WeightAPI is the backend api
 type WeightAPI struct {
@@ -44,7 +13,7 @@ type WeightAPI struct {
 
 // NewWeightAPI returns a new instance
 func NewWeightAPI() *WeightAPI {
-	return &WeightAPI{db: "test.db", driver: "sqlite3"}
+	return &WeightAPI{driver: "sqlite3", db: "test.db"}
 }
 
 // Current returns the last inserted value
@@ -56,11 +25,10 @@ func (w *WeightAPI) Current() (Record, error) {
 	}
 	defer db.Close()
 
-	row := db.QueryRow("select * from trend order by created desc limit 0, 1")
-
+	row := db.QueryRow("select id, datetime(created), value from trend order by created desc limit 0, 1")
 	err = row.Scan(&r.ID, &r.Created, &r.Value)
 
-	log.Println("Current: ", r)
+	log.Println("Current: ", r.String())
 	return r, err
 }
 
@@ -80,6 +48,42 @@ func (w *WeightAPI) Add(r Record) error {
 
 	y, _ := x.LastInsertId()
 	r.ID = int(y)
-	log.Println("Add: ", r)
+	log.Println("Add: ", r.String())
 	return nil
+}
+
+// Last returns the latest n Elements in the thred table
+func (w *WeightAPI) Last(n int) ([]Record, error) {
+	db, err := sql.Open(w.driver, w.db)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	rows, err := db.Query("select id, datetime(created), value  from trend order by created desc limit 0, ?", n)
+	if err != nil {
+		return nil, err
+	}
+
+	var result []Record
+	for rows.Next() {
+		var r Record
+		err = rows.Scan(&r.ID, &r.Created, &r.Value)
+		if err != nil {
+			log.Printf("error while query last items: %s\n", err.Error())
+			continue
+		}
+		result = append(result, r)
+	}
+	return result, nil
+}
+
+// Min returns the minimum value in the trend table
+func (w *WeightAPI) Min() (r Record, err error) {
+	return Record{ID: 23, Value: 23.0}, nil
+}
+
+// Max returns the maximum value in the trend table
+func (w *WeightAPI) Max() (Record, error) {
+	return Record{ID: 42, Value: 42.0}, nil
 }
---
+++ record.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+// Record holds a current weight
+type Record struct {
+	ID      int
+	Created time.Time
+	Value   float32
+}
+
+func (r *Record) String() string {
+	return fmt.Sprintf("%d - %.2f (%s)", r.ID, r.Value, r.Created.Format(time.UnixDate))
+}
+
+// ParseRecord returns a fully parsed record based on the input string
+// and adds the created member
+func ParseRecord(value string) (r Record, err error) {
+	if value == "" {
+		return r, errors.New("empty value")
+	}
+	x, err := strconv.ParseFloat(value, 32)
+	if err != nil {
+		return r, err
+	}
+
+	r.Created = time.Now()
+	r.Value = float32(x)
+	return
+}
